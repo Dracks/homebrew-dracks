d19b1a78db159b7ca64d2bf53fd387418e1a557c
Jaume Singla <jaume.singlavalls@gmail.com>
2026-05-28 07:40:01 +0200

    Update kp2f to 1.0.1

--- Formula/keepass-2-file-bin.rb
+++ Formula/keepass-2-file-bin.rb
@@ -6,21 +6,21 @@ class Keepass2FileBin < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dracks/keepass-2-file/releases/download/v1.0.0/keepass-2-file-1.0.0-aarch64-apple-darwin.tar.gz"
-      sha256 "9b4f4ef65fc626041ba4039cabf029ded1f87810da92c99871f0d3f2b38fde4b"
+      url "https://github.com/Dracks/keepass-2-file/releases/download/v1.0.1/keepass-2-file-1.0.1-aarch64-apple-darwin.tar.gz"
+      sha256 "93df3324c284f94839834b4ff819c9d10ae7d85526e2d9dab087b34c8b170869"
 
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Dracks/keepass-2-file/releases/download/v1.0.0/keepass-2-file-1.0.0-x86_64-apple-darwin.tar.gz"
-      sha256 "44364367ca6d1429aa51371414b3ec6b7b4781b5dd7778a1f744d27a38020454"
+      url "https://github.com/Dracks/keepass-2-file/releases/download/v1.0.1/keepass-2-file-1.0.1-x86_64-apple-darwin.tar.gz"
+      sha256 "65f5b9020580bee129ec7b5ebb35bcca9b95d499af60fa3f5ce1f239c3860819"
 
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Dracks/keepass-2-file/releases/download/v1.0.0/keepass-2-file-1.0.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "4275a0b7de2e31389f24ae11f92638308228e45e2032f88092043a8dd8fc4cdf"
+      url "https://github.com/Dracks/keepass-2-file/releases/download/v1.0.1/keepass-2-file-1.0.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "1e264a2f2c4a9e51276878d80e724ea75953876e538f03341478764c915d00e9"
 
     end
   end
